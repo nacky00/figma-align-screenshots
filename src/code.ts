aaf6fdef19e6153a61daff4dc578451c5bb505da
nacky00 <nacky00eule@gmail.com>
2023-11-05 18:51:39 +0900

    initial commit

--- src/code.ts
+++ src/code.ts
@@ -1,78 +1,55 @@
+figma.showUI(__html__, { themeColors: true, height: 300 });
 
-
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-
-// This shows the HTML page in "ui.html".
-
-figma.showUI(__html__);
 figma.ui.postMessage({
-    "width": figma.root.getPluginData('width'),
-    "radius": figma.root.getPluginData('radius'),
-    "hex": figma.root.getPluginData('hex')
-  });
+  "width": figma.root.getPluginData('width'),
+  "spacing": figma.root.getPluginData('spacing'),
+});
+
+
+figma.root.getPluginData('width');
+figma.root.getPluginData('spacing');
 
 function clone(val: any) {
   return JSON.parse(JSON.stringify(val))
 }
 
-const hexToRgb = (color = "000") =>
-  Object.fromEntries(
-    ((color.match(/^#?[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/) ? color : "000").replace(
-      /^#?(.*)$/,
-      (_, hex) => (hex.length == 3) ? hex.replace(/./g, "$&$&") : hex,
-    ).match(/../g) ?? []).map((c, i) => ["rgb".charAt(i), parseInt("0x" + c)]),
-  );
+let width_value = 390;
+let spacing_value = 50;
 
-const convertedRgb = ({ r = 1, g = 1, b = 1 }) => ({ r: r / 255, g: g / 255, b: b / 255 });
-
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
+function resizeHeightProportionally(currentWidth, newWidth, currentHeight) {
+  const newHeight = (newWidth / currentWidth) * currentHeight;
+  return newHeight;
+}
 figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'change-arrow-style') {
-    for (const node of figma.currentPage.selection) {
-      if (node.type != "VECTOR") return;
-      node.cornerRadius = msg.radius;
-      node.strokeWeight = msg.width;
+  if (msg.type === 'alignment') {
+    // ページ上の選択したフレーム（画像）のリストを取得します
+    const selection = figma.currentPage.selection;
+
+    // 選択された要素があるか確認します
+    if (selection.length > 0) {
+      // 最も左上の要素のX座標を取得します
+      let minX = selection[0].x;
+      const y = selection[0].y;
+      const newWidth = msg.width ? msg.width : width_value;
+      const spacing = msg.spacing ? msg.spacing : spacing_value;
       
-      const clone_strokes = clone(node.strokes);
-      const color = convertedRgb(hexToRgb(msg.color));
-      clone_strokes[0].color = color;
-      
-      node.strokes = clone_strokes;
-      const clone_vector = clone(node.vectorNetwork);
-      clone_vector.vertices[0].strokeCap = "ROUND";
-      clone_vector.vertices[clone_vector.vertices.length - 1].strokeCap = "ARROW_LINES";
-      node.vectorNetwork = clone_vector;
-      
-      figma.root.setPluginData('hex', msg.color);
-      figma.root.setPluginData('width', String(msg.width));
-      figma.root.setPluginData('radius', String(msg.radius));
-    };
-  }
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+      // 選択されたすべての要素に対して処理を行います
+      for (const node of selection) {
+        if (node.type === "RECTANGLE") {
+        const newHeight = resizeHeightProportionally(node.width, newWidth, node.height);
+        // 幅を変更します
+        node.resize(newWidth, newHeight);
+        // X座標を設定して要素を横一列に並べます
+        node.x = minX;
+        node.y = y;
+        // 次の要素のX座標を計算します（余白を考慮）
+        minX += newWidth + spacing;
+      }
+      // プラグインパネルにメッセージを表示します
+      figma.root.setPluginData('width', String(newWidth));
+      figma.root.setPluginData('spacing', String(spacing));
     }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+  } else {
+    figma.notify('画像を選択してから実行してください')
   }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  // figma.closePlugin();
-};
-
-
+}};
--- src/ui.ts
+++ src/ui.ts
@@ -1,28 +1,25 @@
 import './ui.css'
 
-const widthInit = "5";
-const radiusInit = "12";
-const hexInit = '000000';
+const widthInit = "390";
+const spacingInit = "100";
 
 const widthElement = document.getElementById('width') as HTMLInputElement;
-const radiusElement = document.getElementById('radius') as HTMLInputElement;
-const colorElement = document.getElementById('color') as HTMLInputElement;
+const spacingElement = document.getElementById('spacing') as HTMLInputElement;
+const ChangeBtn = document.getElementById('change') as HTMLInputElement;
 
-let preValue = (preData) => preData ? preData : { "width": widthInit, "radius": radiusInit, "hex": hexInit }
-
-onmessage = (event) => {
-    widthElement.value = preValue(event.data.pluginMessage).width as string;
-    radiusElement.value = preValue(event.data.pluginMessage).radius as string;
-    colorElement.value = preValue(event.data.pluginMessage).hex as string;
-}
+let preValue = (preData) => preData.width !== "" && preData.spacing !== "" ? preData : { "width": widthInit, "spacing": spacingInit }
 
 widthElement.onchange = () => { widthElement.value = widthElement.value == "" ? widthElement.value = widthInit : widthElement.value; }
-radiusElement.onchange = () => { radiusElement.value = radiusElement.value == "" ? radiusElement.value = radiusInit : radiusElement.value; }
-colorElement.onchange = () => { colorElement.value = colorElement.value == "" ? colorElement.value = hexInit : colorElement.value; }
+spacingElement.onchange = () => { spacingElement.value = spacingElement.value == "" ? spacingElement.value = spacingInit : spacingElement.value; }
 
-document.getElementById('change').onclick = () => {
+ChangeBtn.onclick = () => {
     const width = parseInt(widthElement.value, 10);
-    const radius = parseInt(radiusElement.value, 10);
-    const color = colorElement.value;
-    parent.postMessage({ pluginMessage: { type: 'change-arrow-style', width, radius, color } }, '*')
+    const spacing = parseInt(spacingElement.value, 10);
+    parent.postMessage({ pluginMessage: { type: 'alignment', width, spacing } }, '*')
+}
+
+// コードの最後に呼び出す
+onmessage = (event) => {
+    widthElement.value = preValue(event.data.pluginMessage.width);
+    spacingElement.value = preValue(event.data.pluginMessage.spacing);
 }
